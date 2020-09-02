f4f536684bc2d8442a3e1ecd065172e164a3ebce
larse514 <andrew.larse514@gmail.com>
2020-09-02 18:56:05 -0500

    add todo list with in memory db

---
+++ dot-net-core-template/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace dot_net_core_template
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ dot-net-core-template/Models/TodoListContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace dot_net_core_template
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
